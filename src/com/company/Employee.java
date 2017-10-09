40b0dab137121a37509bad8bcffec0ce836e4fb8
TheLostKing <calderonezj@gmail.com>
2017-10-08 22:49:00 -0500

    Finished

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,13 +1,22 @@
 package com.company;
+enum Position{
+    Administration, Production, Sales, Maintenance, Technical, Secretarial
+}
 
 public class Employee {
-    String employeeFirstName, employeeLastName;
+    String employeeFirstName, employeeLastName, startDate;
     int shift, employeeNumber;
+    boolean salary;
+    double payRate, pay;
+    Position position;
 
-    public Employee(String first, String last, int shift, int num){
+    public Employee(String first, String last, Position position, boolean salary, double payRate, int shift, String startDate){
         setName(first, last);
         setShift(shift);
-        setEmployeeNumber(num);
+        setPosition(position);
+        setSalary(salary);
+        setPayRate(payRate);
+        setStartDate(startDate);
     }
 
     public String getEmployeeFirstName() {
@@ -47,12 +56,70 @@ public class Employee {
         return employeeNumber;
     }
 
+    public Position getPosition() {
+        return position;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public boolean isSalary() {
+        return salary;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
     public void setEmployeeNumber(int employeeNumber) {
         this.employeeNumber = employeeNumber;
     }
 
-    @Override
-    public String toString() {
-        return "Employee Name: " + getName() + "\nEmployee Number: " + getEmployeeNumber() + "\nEmployee Shift: " + getShift() + "\n";
+    public void setSalary(boolean salary) {
+        this.salary = salary;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+    public double calculate(double hoursIn){
+        if(salary){
+            pay = payRate*hoursIn;
+        }else{
+            if(hoursIn > 40){
+                pay = payRate*40 + (hoursIn-40)*payRate*1.5;
+            }else{
+                pay = payRate*hoursIn;
+            }
+        }
+
+        switch(shift) {
+            case 1:
+                pay*= 1;
+                break;
+            case 2:
+                pay *= 1.05;
+                break;
+            case 3:
+                pay *= 1.1;
+                break;
+            default:
+                System.out.println("Someone fucked up here");
+                break;
+        }
+        return pay;
+    }
+
+    public String toString(double hoursIn) {
+        return "Employee Name: " + getName() + "\nEmployee Shift: " + getShift() + "\nPosition: " + getPosition() + "\nPay Rate: " + getPayRate() + "\nThis Weeks Pay: " + calculate(hoursIn) +"\n";
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        Employee employee1 = new Employee("Zac", "Calderone", 2, 2095421);
-        Employee employee2 = new Employee("Arthas", "Menethil", 3, 1234567);
-        Employee employee3 = new Employee("John", "Shepard", 1, 76509832);
+        Employee employee1 = new Employee("Zac", "Calderone", Position.Secretarial, false, 30, 3, "10/03/2003");
+        Employee employee2 = new Employee("Arthas", "Menethil", Position.Maintenance, true, 25, 1, "08/10/2001");
+        Employee employee3 = new Employee("John", "Shepard", Position.Sales, true, 28, 2, "07/14/2015");
 
-        System.out.println(employee1);
-        System.out.println(employee2);
-        System.out.println(employee3);
+        System.out.println(employee1.toString(40));
+        System.out.println(employee2.toString(55));
+        System.out.println(employee3.toString(60));
     }
 }
